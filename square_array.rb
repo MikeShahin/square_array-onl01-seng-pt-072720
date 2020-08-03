2bd2f774789788231674534a99aa660a5348484e
Mike Shahindoust <gobias101@gmail.com>
2020-08-03 22:42:31 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,6 @@ numbers = [1, 2, 3]
 new_nums = []
 
 def square_array(array)
-  # your code here
-  #numbers.each do |number|
-   # new_nums = number^2 
-#end 
+
   puts "#{new_nums} and #{number}"
 end
