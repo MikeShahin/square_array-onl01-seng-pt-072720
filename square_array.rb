6c1e4fcc2e7567096c9a371f09c45abf940b7b91
Mike Shahindoust <gobias101@gmail.com>
2020-08-03 22:48:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 
-#def square_array(array)
 #numbers = [1, 2, 3]
 #new_nums = []
 #  puts "#{new_nums} and #{number}"
-#end
 
+def square_array(array)
 a = [ 1, 2, 3 ]
 a.each {|x| print (x*x), " -- " }
+end
