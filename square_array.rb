e1d25d64fda0bf66ee2084b3086d59c14f9c5825
Mike Shahindoust <gobias101@gmail.com>
2020-08-03 23:04:55 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -5,11 +5,11 @@ new_nums = []
 new_array = []
 def square_array(array)
   array.each do |x|
-    new_num = x*x
+    new_nums = x*x
     #puts new_num
   end  
   #new_array = array.each {|x| x*4}
-  #puts new_array
+ # puts new_nums
 end
 
 square_array([1, 2, 3])
